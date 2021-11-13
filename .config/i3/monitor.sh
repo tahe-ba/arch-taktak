5017b20068585ad0d29abc472c3f2b270b870d02
tahe-ba <taha.ben.a@outlook.com>
2021-11-13 21:06:55 +0100

    aaa

---
+++ .config/i3/monitor.sh
@@ -0,0 +1,4 @@
+xrandr --newmode "1440x1080_60.00"  129.87  1440 1536 1688 1936  1080 1081 1084 1118  -HSync +Vsync
+xrandr --addmode HDMI1 "1440x1080_60.00"
+xrandr --auto --output HDMI1 --mode 1440x1080_60.00 --left-of eDP1
+
