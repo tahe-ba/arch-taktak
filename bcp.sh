6b21adab52a60a6d4fc3335683f7515ab7169ed4
tahe-ba <taha.ben.a@outlook.com>
2021-11-21 03:18:05 +0100

    wiiii

--- bcp.sh
+++ bcp.sh
@@ -1,5 +1,5 @@
 cp /home/taktak/.config/picom.conf .config/picom.conf
-cp -R /home/taktak/.config/i3 .config/i3
+cp -R /home/taktak/.config/i3 .config/
 cp /home/taktak/.bashrc .bashrc
 cp /home/taktak/.alacritty.yml .alacritty.yml
 
