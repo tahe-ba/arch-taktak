d4bd22139499c2aa3f89bd72bbcb752c893825d3
tahe-ba <taha.ben.a@outlook.com>
2021-11-21 03:16:35 +0100

    wiiii

---
+++ bcp.sh
@@ -0,0 +1,8 @@
+cp /home/taktak/.config/picom.conf .config/picom.conf
+cp -R /home/taktak/.config/i3 .config/i3
+cp /home/taktak/.bashrc .bashrc
+cp /home/taktak/.alacritty.yml .alacritty.yml
+
+git add *
+git commit -m "wiiii"
+git push 
