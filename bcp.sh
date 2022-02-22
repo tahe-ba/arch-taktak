66f05d8fb7bd0c6e468ceacad05f8d0c254e67bf
tahe-ba <taha.ben.a@outlook.com>
2022-02-22 17:54:09 +0100

    wiiii

--- bcp.sh
+++ bcp.sh
@@ -2,7 +2,7 @@ cp /home/taktak/.config/picom.conf .config/picom.conf
 cp -R /home/taktak/.config/i3 .config/
 cp /home/taktak/.bashrc .bashrc
 cp /home/taktak/.alacritty.yml .alacritty.yml
-
+cp /home/taktak/.config/libinput-gestures.conf .config/libinput-gestures.conf
 git add *
 git add .
 git commit -m "wiiii"
